1e4a1c9b205c8bb0431831ef9075143c99d3b12a
jasbo-777 <jasminbains7@gmail.com>
2025-01-22 13:50:42 -0800

    Add files via upload

---
+++ VarsAndMath.java
@@ -0,0 +1,18 @@
+
+public class VarsAndMath {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 1.75;
+        char initial = 'J';
+        boolean isStudent = true;
+
+        int nextYearAge = age + 1;
+        double heightInFeet = height * 3.281;
+
+        String greeting = "Hello, my name is " + initial + ".";
+        String info = "I am " + age + " years old and " + height + " meters tall.";
+
+        System.out.println(greeting);
+        System.out.println(info);
+        }
+        }
